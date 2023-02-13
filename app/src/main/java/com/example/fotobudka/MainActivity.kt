9123153c5a3ac0850d3c14dbe7ece80f247b07eb
Loonk112 <104651234+Loonk112@users.noreply.github.com>
2023-02-13 17:22:45 +0100

    ///

---
+++ app/src/main/java/com/example/fotobudka/MainActivity.kt
@@ -0,0 +1,224 @@
+package com.example.fotobudka
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import android.media.ImageReader
+import android.media.MediaActionSound
+import android.os.Bundle
+import android.os.Environment
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.*
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var capReq: CaptureRequest.Builder
+    lateinit var handler: Handler
+    lateinit var handlerThread: HandlerThread
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+    lateinit var cameraCaptureSession: CameraCaptureSession
+    lateinit var cameraDevice: CameraDevice
+    lateinit var imageReader: ImageReader
+
+    var seriesName = ""
+    var targetCount: Int = 0
+    var delay = 0
+    var count: Int = 0
+    var job: Job? = null
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraDevice.close()
+        handler.removeCallbacksAndMessages(null)
+        handlerThread.quitSafely()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        get_permissions()
+
+        textureView = findViewById(R.id.cameraPreview)
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler((handlerThread).looper)
+
+        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                open_camera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+
+            }
+
+        }
+
+
+        imageReader = ImageReader.newInstance(1080,1920, ImageFormat.JPEG, 1)
+        imageReader.setOnImageAvailableListener(object: ImageReader.OnImageAvailableListener{
+            override fun onImageAvailable(reader: ImageReader?) {
+
+
+                var image = reader?.acquireLatestImage()
+
+                var buffer = image!!.planes[0].buffer
+                var bytes = ByteArray(buffer.remaining())
+                buffer.get(bytes)
+
+
+                var file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "$seriesName$count.jpeg")
+
+                var opStream = FileOutputStream(file)
+
+                opStream.write(bytes)
+
+                opStream.close()
+                image.close()
+
+                val sound = MediaActionSound()
+                sound.play(MediaActionSound.SHUTTER_CLICK)
+            }
+        }, handler)
+
+        val countIn =findViewById<EditText>(R.id.countIn)
+        val delayIn =findViewById<EditText>(R.id.timeIn)
+        val nameIn =findViewById<EditText>(R.id.nameIn)
+
+        findViewById<FloatingActionButton>(R.id.floatingActionButton).apply {
+            setOnClickListener {
+                targetCount = countIn.text.toString().toInt()
+                delay = (delayIn.text.toString().toDouble() * 1000).toInt()
+                seriesName = nameIn.text.toString()
+
+                if (targetCount > 0 && delay >= 100 && seriesName != "") {
+
+                    val currentJob = job
+                    if (currentJob == null || currentJob.isCompleted) {
+                        job = GlobalScope.launch(Dispatchers.IO) {
+                            for (i in 0..targetCount) {
+                                count = i
+                                delay(delay.toLong())
+                                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+                                capReq.addTarget(imageReader.surface)
+                                cameraCaptureSession.capture(capReq.build(),null,null)
+
+                            }
+                            seriesName = ""
+                        }
+                    } else {
+                        Toast.makeText(context, "Photo taking in progress...", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(context, "Wrong inputs", Toast.LENGTH_SHORT).show()
+                }
+
+
+            }
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    fun open_camera() {
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+
+                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                var surface = Surface(textureView.surfaceTexture)
+                capReq.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface, imageReader.surface), object :
+                    CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        cameraCaptureSession = session
+                        cameraCaptureSession.setRepeatingRequest(capReq.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+
+                    }
+                }, handler)
+            }
+
+            override fun onClosed(camera: CameraDevice) {
+                super.onClosed(camera)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+
+            }
+        }, handler)
+    }
+
+    fun get_permissions() {
+        var permissionsList = mutableListOf<String>()
+
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(android.Manifest.permission.CAMERA)
+        }
+        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        if (permissionsList.size > 0) {
+            requestPermissions(permissionsList.toTypedArray(), 101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        grantResults.forEach {
+            if (it != PackageManager.PERMISSION_GRANTED) {
+                get_permissions()
+            }
+        }
+    }
+}
